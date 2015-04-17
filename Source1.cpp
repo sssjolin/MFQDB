b6f2804cd7e3c24006432133d55d056c058abb22
hqu1 <hqu1@stevens.edu>
2015-04-17 17:09:22 -0400

    Signed-off-by: hqu1 <hqu1@stevens.edu>

---
+++ Source1.cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+#include <pqxx/pqxx>  
+
+using namespace std;
+using namespace pqxx;
+
+int main(int argc, char* argv[])
+{
+    try{
+        connection C("dbname=cs562 user=cs562 password=cs562final \ 
+            hostaddr = 128.199.74.194 port = 5432"); 
+            if (C.is_open()) {
+            cout << "Opened database successfully: " << C.dbname() << endl;
+            }
+            else {
+                cout << "Can't open database" << endl;
+                return 1;
+            }
+            C.disconnect();
+    }
+    catch (const std::exception &e){
+        cerr << e.what() << std::endl;
+        return 1;
+    }
+}
